a87c7dc74ae63a09f5409e6d55f2ac85f397fd2f
Stanislav Sazonov <slavk11@yandex.ru>
2023-05-05 22:40:53 +0300

    created model for JSON

---
+++ Networking/Anime.swift
@@ -0,0 +1,12 @@
+//
+//  Anime.swift
+//  Networking
+//
+//  Created by Сазонов Станислав on 05.05.2023.
+//
+
+struct Anime: Decodable {
+    let anime: String
+    let character: String
+    let quote: String
+}
