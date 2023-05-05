d95fc07a660aa5ceb88460db3937758a6e1741c8
Stanislav Sazonov <slavk11@yandex.ru>
2023-05-05 22:41:02 +0300

    parsing json

--- Networking/MainViewController.swift
+++ Networking/MainViewController.swift
@@ -8,7 +8,32 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-
+    private let urlAddress = "https://animechan.vercel.app/api/quotes/anime?title=naruto"
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchAnime()
+    }
+    
+    private func fetchAnime() {
+        guard let url = URL(string: urlAddress) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+                
+            }
+            do {
+                let decoder = JSONDecoder()
+                let anime = try decoder.decode([Anime].self, from: data)
+                print(anime)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
 }
+
 
