17baba7801fcc4f151206c8260521db844f74095
Stanislav Sazonov <slavk11@yandex.ru>
2023-05-05 19:34:11 +0300

    Created started class

---
+++ Networking/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Сазонов Станислав on 05.05.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    
+}
+
